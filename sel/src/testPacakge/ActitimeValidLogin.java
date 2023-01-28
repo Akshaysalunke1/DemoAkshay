51f2b3674b81e7e25b7b3d2f829a20ffd3e3963c
DELL <DELL@DESKTOP-EB5LMEM>
2023-01-28 14:16:13 +0530

    kuhi

---
+++ sel/src/testPacakge/ActitimeValidLogin.java
@@ -0,0 +1,16 @@
+package testPacakge;
+
+import org.testng.annotations.Test;
+
+import genericPacage.BaseTest;
+
+public class ActitimeValidLogin extends BaseTest {
+	
+	@Test
+public void validTest()
+{
+		
+}
+}
+
+
